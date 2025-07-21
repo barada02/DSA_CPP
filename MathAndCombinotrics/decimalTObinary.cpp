2cd7c0f04d3994cda310b2c3ab5be0b1a29d41f4
Chandan_Kumar <chandanbarada2@gmail.com>
2025-07-22 00:47:56 +0530

    decimal

---
+++ MathAndCombinotrics/decimalTObinary.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+//Given a non negative number n, the task is to convert the given number into an equivalent binary representation.
+
+
+int main() {
+    
+    
+    return 0;
+}
