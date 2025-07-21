e3d0d5a124a938cfd08e37c09d3973c55a33a780
Chandan_Kumar <chandanbarada2@gmail.com>
2025-07-22 00:48:12 +0530

    approach1

--- MathAndCombinotrics/decimalTObinary.cpp
+++ MathAndCombinotrics/decimalTObinary.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 //Given a non negative number n, the task is to convert the given number into an equivalent binary representation.
 
+//[Approach - 1] Division by 2 - O(log₂(n)) Time and O(log₂(n)) Space
+
 
 int main() {
     
