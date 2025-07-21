e7347fed411fd945bfd4d7c3e214c526976cf345
Chandan_Kumar <chandanbarada2@gmail.com>
2025-07-21 23:27:37 +0530

    floor and ceil first method

---
+++ MathAndCombinotrics/floreAndCeil.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+Given two integers a and b (where b ≠ 0). The objective is to find:
+• The floor of the division a / b, denoted as ⌊a / b⌋ — the greatest integer less than or equal to a / b.
+• The ceil of the division a / b, denoted as ⌈a / b⌉ — the smallest integer greater than or equal to a / b.
+The floor and ceil functions are commonly used in mathematics and computer science to round numbers down or up, respectively.
+*/
+
+int floor(int a, int b){
+    if (b == 0) {
+        throw invalid_argument("Division by zero is not allowed.");
+    }
+    return a / b - (a % b < 0 ? 1 : 0); 
+}
+
+int floor2(int a, int b){
+    
+}
+
+int ceil(int a, int b){
+    if (b == 0) {
+        throw invalid_argument("Division by zero is not allowed.");
+    }
+    return a / b + (a % b > 0 ? 1 : 0);
+}
