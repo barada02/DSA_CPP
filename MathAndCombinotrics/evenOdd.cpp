5a1c0d45090ab306cd607477e8bca8c2081fd07a
Chandan_Kumar <chandanbarada2@gmail.com>
2025-07-21 23:15:40 +0530

    Note

--- MathAndCombinotrics/evenOdd.cpp
+++ MathAndCombinotrics/evenOdd.cpp
@@ -15,6 +15,13 @@ bool isEven(int n){
    Optimized approach to check if a number is even or odd
    using bitwise operator
 */
+/*
+The last bit of all odd numbers is always 1, while for even numbers it’s 0. 
+So, when performing bitwise AND operation with 1, odd numbers give 1, and even numbers give 0.
+
+Note: Bitwise operators are extremely fast and efficient because they operate directly at the binary level, 
+making them significantly faster than arithmetic or logical operations.
+*/
 bool isEven2(int n){
     if((n&1)==0){
         return true;
